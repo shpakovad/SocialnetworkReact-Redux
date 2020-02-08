5b687a2237d7e9f45e90b5723d6b4de039878501
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-02-08 19:44:41 +0300

    Updates

---
+++ precache-manifest.6ab297118a9343ab58b17d1b06268268.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9687dadb83634cb7644f2412a6df3ec2",
+    "url": "/SocialnetworkReact-Redux/index.html"
+  },
+  {
+    "revision": "3fe06c6c6172cdf5c95a",
+    "url": "/SocialnetworkReact-Redux/static/css/3.3336339d.chunk.css"
+  },
+  {
+    "revision": "30a0270a4b96c6398473",
+    "url": "/SocialnetworkReact-Redux/static/css/4.fd5f2de6.chunk.css"
+  },
+  {
+    "revision": "52a43310cf0229f97cc4",
+    "url": "/SocialnetworkReact-Redux/static/css/main.116e3974.chunk.css"
+  },
+  {
+    "revision": "5414189efe2f31570dd3",
+    "url": "/SocialnetworkReact-Redux/static/js/2.a5b7875a.chunk.js"
+  },
+  {
+    "revision": "3fe06c6c6172cdf5c95a",
+    "url": "/SocialnetworkReact-Redux/static/js/3.373d8c2c.chunk.js"
+  },
+  {
+    "revision": "30a0270a4b96c6398473",
+    "url": "/SocialnetworkReact-Redux/static/js/4.a90afe26.chunk.js"
+  },
+  {
+    "revision": "52a43310cf0229f97cc4",
+    "url": "/SocialnetworkReact-Redux/static/js/main.fe1a4c1e.chunk.js"
+  },
+  {
+    "revision": "0adb0264618caaddabd0",
+    "url": "/SocialnetworkReact-Redux/static/js/runtime~main.89429b19.js"
+  },
+  {
+    "revision": "2fa419313e38ff018e4585d92f93854f",
+    "url": "/SocialnetworkReact-Redux/static/media/bg_image.2fa41931.jpg"
+  },
+  {
+    "revision": "26bce32a4e244b379d064b20b3157479",
+    "url": "/SocialnetworkReact-Redux/static/media/cat.26bce32a.svg"
+  },
+  {
+    "revision": "8d818e4ec043d1ef018a187084e79e4a",
+    "url": "/SocialnetworkReact-Redux/static/media/like.8d818e4e.svg"
+  },
+  {
+    "revision": "5a2a761058ceb940c9b3e3f7a4b4dd81",
+    "url": "/SocialnetworkReact-Redux/static/media/preloader.5a2a7610.svg"
+  },
+  {
+    "revision": "9306807ee169abb1f9c19b187d84a45c",
+    "url": "/SocialnetworkReact-Redux/static/media/userPhoto.9306807e.svg"
+  },
+  {
+    "revision": "a8bb2c7f2ed5a40ababc894526bbd858",
+    "url": "/SocialnetworkReact-Redux/static/media/userPhotoDialogs.a8bb2c7f.svg"
+  }
+]);
